4ca634edec6893651c28fb0653629541bb8bc9b4
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-07 13:37:25 +0200

    fetches data from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import './App.css'
 import Header from './components/ui/Header'
 
+const BASE_URL = 'https://www.breakingbadapi.com/api/characters'
+
 function App() {
+  const [characters, setCharacters] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(async () => {
+    const response = await axios(BASE_URL)
+    setCharacters(response.data)
+    setIsLoading(false)
+  }, [])
+
+  console.log(characters)
   return (
     <div className='App'>
       <Header />
